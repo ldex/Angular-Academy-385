2949cc9a2641b3a4db5dca008594448bfdd2fabf
ldex <laurent.duveau+git@gmail.com>
2023-11-27 15:57:04 -0500

    Service Http

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { Product } from '../../models/product.interface';
 import { ProductDetailComponent } from '../product-detail/product-detail.component';
 import { ProductService } from '../../services/product.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-product-list',
@@ -14,12 +15,10 @@ import { ProductService } from '../../services/product.service';
 export class ProductListComponent {
   title: string = 'Products'
   selectedProduct: Product;
-
   private productService = inject(ProductService);
+  products$: Observable<Product[]> = this.productService.products$;
 
   onSelect(product: Product): void {
     this.selectedProduct = product;
   }
-
-  products: Product[] = this.productService.getProducts();
 }
