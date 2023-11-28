bfed83b08f52df54d6e0ccad45b4436ff3a9d7f6
ldex <laurent.duveau+git@gmail.com>
2023-11-28 15:38:16 -0500

    Product Delete

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { Product } from '../../models/product.interface';
 import { ProductService } from '../../services/product.service';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-detail',
@@ -13,11 +14,30 @@ import { Observable } from 'rxjs';
 })
 export class ProductDetailComponent {
 
-  private productService = inject(ProductService);
+  private productService = inject(ProductService)
+  private router = inject(Router)
 
   product$: Observable<Product>;
+  private productId: number;
 
+  deleteProduct() {
+    this
+      .productService
+      .deleteProduct(this.id)
+      .subscribe(
+        () => {
+          console.log('Product deleted on the server.');
+          this.productService.resetCache();
+          this.router.navigateByUrl('/products');
+        }
+      )
+  }
+
+  get id(): number {
+    return this.productId;
+  }
   @Input() set id(productId) {
+    this.productId = productId;
     this.product$ = this
                       .productService
                       .getProductById(productId)
